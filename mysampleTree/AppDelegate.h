1bd88100f0590067a18644c8e2836dfdee8e6fd2
Prajas <prajas@Prajass-Mac-Pro.local>
2014-04-10 16:19:56 +0530

    Initial Commit

---
+++ mysampleTree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mysampleTree
+//
+//  Created by Prajas on 4/10/14.
+//  Copyright (c) 2014 Prajas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
